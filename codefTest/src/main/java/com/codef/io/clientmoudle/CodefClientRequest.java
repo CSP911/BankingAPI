cd7419da7597feff85a8bf9f75a7bec2d2901089
dnk <cromara1695@gmail.com>
2023-01-14 19:05:16 +0900

    working now

--- codefTest/src/main/java/com/codef/io/clientmoudle/CodefClientRequest.java
+++ codefTest/src/main/java/com/codef/io/clientmoudle/CodefClientRequest.java
@@ -1,4 +1,5 @@
 package com.codef.io.clientmoudle;
+
 import com.codef.io.util.RSAUtil;
 
 import java.io.IOException;
@@ -17,8 +18,12 @@ import org.json.simple.parser.ParseException;
 import com.codef.io.util.ApiRequest;
 import com.codef.io.util.CommonConstant;
 
-public class CodefClientRequest{
+public class CodefClientRequest {
     private String urlPath;
+
+    private String connectedID;
+    private String purpose;
+
     private String id;
     private String password;
     private String fastId;
@@ -32,42 +37,47 @@ public class CodefClientRequest{
     private String identity;
     private String smsAuthNo;
 
-    private String connectedID;
-    private String purpose;
-    
 
-
-    public CodefClientRequest(JSONObject data){
+    public CodefClientRequest(JSONObject data) {
         urlPath = CommonConstant.getRequestDomain();
-        //+CommonConstant.KR_BK_1_B_007;
-        purpose=data.getString("purpose");
-        switch(purpose){
-            case "AccountInquiry":
-                urlPath += CommonConstant.KR_BK_1_B_001;
-            case "FastInquiry":
-                urlPath += CommonConstant.KR_BK_1_B_007;
-                id = data.getString("id");
-                password = data.getString("password");
-                fastId = data.getString("fastId");
-                fastPassword = data.getString("fastPassword");
-                organization = data.getString("organization");
-                account = data.getString("account");
-                accountPassword = data.getString("accountPassword");
-                startDate = data.getString("startDate");
-                endDate = data.getString("endDate");
-                orderBy = data.getString("orderBy");
-                identity = data.getString("identity");
-                smsAuthNo = data.getString("smsAuthNo");
-            break;
-        }
+        if (data.has("purpose"))
+            purpose = data.getString("purpose");
+        if (data.has("organization"))
+            organization = data.getString("organization");
+        if (data.has("id"))
+            id = data.getString("id");
+        if (data.has("password"))
+            password = data.getString("password");
+        if (data.has("fastId"))
+            fastId = data.getString("fastId");
+        if (data.has("fastPassword"))
+            fastPassword = data.getString("fastPassword");
+        if (data.has("account"))
+            account = data.getString("account");
+        if (data.has("accountPassword"))
+            accountPassword = data.getString("accountPassword");
+        if (data.has("startDate"))
+            startDate = data.getString("startDate");
+        if (data.has("endDate"))
+            endDate = data.getString("endDate");
+        if (data.has("orderBy"))
+            orderBy = data.getString("orderBy");
+        if (data.has("identity"))
+            identity = data.getString("identity");
+        if (data.has("smsAuthNo"))
+            smsAuthNo = data.getString("smsAuthNo");
     }
 
     public String requestCodefData() throws IOException, InterruptedException, ParseException, InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException{
         HashMap<String, Object> bodyMap = new HashMap<String, Object>();
         switch(purpose){
             case "AccountInquiry":
+                urlPath += CommonConstant.KR_BK_1_B_001;
+                bodyMap.put("connectedId",connectedID);	// 엔드유저의 은행/카드사 계정 등록 후 발급받은 커넥티드아이디 예시
+                bodyMap.put("organization",organization);
                 break;
             case "FastInquiry":
+                urlPath += CommonConstant.KR_BK_1_B_007;
                 bodyMap.put("id", id);
                 bodyMap.put("password", password);
                 bodyMap.put("fastId", fastId);
@@ -81,6 +91,8 @@ public class CodefClientRequest{
                 bodyMap.put("identity",identity);
                 bodyMap.put("smsAuthNo", smsAuthNo);
                 break;
+            default:
+                return null;
          // 요청 파라미터 설정 종료
         }
         // API 요청
